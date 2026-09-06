5de71f5b01ab1b9574a0ac75ec261909ae4bb76d
Hiing77 <supiii77@hotmail.com>
2026-09-06 21:53:43 +0200

    feat: ajout de mes exercices sql

---
+++ 10 - SQL/v2/sql2/database/init.sql
@@ -0,0 +1,5 @@
+INSERT INTO produit (id_produit, nom, categorie, quantite) VALUES 
+(1, 'Clavier', 'Accessoire', 10),
+(2, 'Souris', 'Accessoire', 25),
+(3, 'Écran', 'Matériel', 5),
+(4, 'PC Portable', 'Matériel', 3);
